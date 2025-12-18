04265259a6b0f5fcef81099baacdcf695c90e7ad
Davut Oral <infwpj201@ad-ailab.de>
2025-12-18 11:32:28 +0100

    code eingebunden

--- Programs/OneWireBus/Src/main.c
+++ Programs/OneWireBus/Src/main.c
@@ -1,8 +1,8 @@
 /**
   ******************************************************************************
   * @file    main.c
-  * @author  
-  * @brief   
+  * @author  Franz Korf
+  * @brief   Kleines Testprogramm fuer neu erstelle Fonts.
   ******************************************************************************
   */
 /* Includes ------------------------------------------------------------------*/
@@ -19,6 +19,18 @@
 
 
 int main(void) {
+	initITSboard();    // Initialisierung des ITS Boards
+	
+	GUI_init(DEFAULT_BRIGHTNESS);   // Initialisierung des LCD Boards mit Touch
+	TP_Init(false);                 // Initialisierung des LCD Boards mit Touch
 
+  // Begruessungstext	
+	lcdPrintlnS("Hallo liebes TI-Labor (c-project)");
+	
+	// Test in Endlosschleife
+	while(1) {
+		HAL_Delay(10000);
+	}
 }
+
 // EOF
